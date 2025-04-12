41de842dc0207ad45c23f66d3be183def823a173
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2025-04-12 11:23:16 +0000

    Nova atualização mensagem principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Amoe, não fica preocupada com os bens materiais, tá bom? Te amo 3000!</h1>
+    return <div style={{ backgroundColor: 'lightblue', textAlign: 'justify'}}>
+        <h1>Amoe</h1>
+        <p style={{ backgroundColor: 'greenyellow'}}>Não fica preocupada com os bens materiais, tá bom? Te amo 3000!</p>
+    </div>
 }
 
 export default Home;
